5d016bff9138f4fa64ca8abfd8f16264c2bddffa
zyb955140 <384815317@qq.com>
2019-04-03 17:53:47 +0800

    优化前端页面

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,7 +28,9 @@ module.exports = {
   ** Global CSS
   */
   css: [
-    'element-ui/lib/theme-chalk/index.css'
+    'element-ui/lib/theme-chalk/index.css',
+    'element-ui/lib/theme-chalk/reset.css',
+   // '~assets/css/main.css'
   ],
 
   /*
@@ -42,14 +44,18 @@ module.exports = {
   ** Nuxt.js modules
   */
   modules: [
+    '@nuxtjs/axios'
   ],
+  axios:{
+
+  },
 
   /*
   ** Build configuration
   */
   build: {
     transpile: [/^element-ui/],
-    
+
     /*
     ** You can extend webpack config here
     */
